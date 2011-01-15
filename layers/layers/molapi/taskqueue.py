7c64974acba386586b4c3902627a92dc53815ac3
andrewxhill <andrewxhill@gmail.com>
2011-01-14 17:50:45 -0700

    mostly debugged

--- layers/layers/molapi/taskqueue.py
+++ layers/layers/molapi/taskqueue.py
@@ -2,36 +2,38 @@ import sys, logging, os, shutil, datetime, time, shlex, subprocess
 import StringIO
 
 class Layer():
-    zoom = 12 #sets the maximum zoom we want to process
+    zoom = 5 #sets the maximum zoom we want to process
     info = {}
     errors = []
-    nulfp= StringIO.StringIO()
-    projected = False
     converted = False
     tiled = False
+    tileDir = "tiles/" #/some/tmp/folder/for/tiles/
+    ascDir = "" #/some/tmp/folder/for/asc/
     
-    def __init__(self, dirname=None,filename=None):
+    
+    def __init__(self, dirname="",filename=None):
         """raster: string filename of file to process"""
         if filename is not None:
-            self.origRaster = "%s/%s" % (dirname,filename)
+            self.origRaster = dirname.rstrip("/") + "/" + filename if dirname != "" else filename
             self.id = filename.split('.')[0]
             self.verifyId()
-            self.getInfo()
-            self.tileFolder = "/some/tmp/folder/for/tiles/" + self.id
-            self.ascName = "/some/tmp/folder/for/%s.asc" % self.id
-            self.tifName = "/some/tmp/folder/for/%s.tif" % self.id
-            
+            self.tileFolder = self.tileDir + self.id
+            self.ascName = self.ascDir + "%s.asc" % self.id
+            self.nulfp= open('%s.log' % self.id,'w')
         
     def verifyId(self):
         """check to see that the id exists on GAE"""
         return True
     
-    def getInfo(self):
+    def getInfo(self,fn):
         #use gdalinfo to populate an info object
         info = subprocess.Popen(
             ["gdalinfo",
-             self.origRaster
-            ], stdout=subprocess.PIPE).communicate()[0].split("\n")
+             fn
+            ], stdout=subprocess.PIPE).communicate()[0]
+        #better parsing needed
+        """
+        info = info.split("\n")
         self.info["id"] = self.id
         self.info["zoom"] = {}
         self.info["box"] = {}
@@ -41,62 +43,33 @@ class Layer():
         
         upper = info[7].split()
         lower = info[10].split()
-        self.info["box"]["xmin"] = upper[3].replace(",",""))
-        self.info["box"]["xmax"] = lower[3].replace(",",""))
-        self.info["box"]["ymax"] = upper[3].replace(",","")).replace(")",""))
-        self.info["box"]["ymin"] = lower[3].replace(",","")).replace(")",""))
-        
+        self.info["box"]["xmin"] = upper[3].replace(",","")
+        self.info["box"]["xmax"] = lower[3].replace(",","")
+        self.info["box"]["ymax"] = upper[3].replace(",","").replace(")","")
+        self.info["box"]["ymin"] = lower[3].replace(",","").replace(")","")
+        """
         #should grab projection info here also
         
         return True
         
-    def projectToGMAP(self):
-        #should add steps to check that the projection isn't already 90013
-        if self.projected:
-            return True
-        else:
-            self.projecting = subprocess.Popen(
-                ["gdalwarp",
-                 "-of",
-                 "GTiff",
-                 "-t_srs",
-                 "epsg:900913",
-                 self.origRaster,
-                 self.tifName
-                ], stderr= self.nulfp)
-            self.projected = True
-            return True
-        
     def convertToASC(self):
-        if not self.projected:
-            self.projectToGMAP()
+        #create a geotiff
+        self.translating = subprocess.Popen(
+            ["gdal_translate",
+            "-of",
+            "AAIGrid",
+            "-a_srs",
+             "epsg:900913",
+             self.origRaster,
+             self.ascName
+            ], stderr=self.nulfp)
+        self.translating.wait()
+        self.converted = True
+        self.getInfo(self.ascName)
         
-        try:
-            self.projecting.wait()
-        except:
-            pass
-            
-        if self.converted:
-            return True
-        else:
-            self.converting = subprocess.Popen(
-                ["gdal_translate",
-                 "-of",
-                 "AAIGrid",
-                 self.tifName,
-                 self.ascName)
-                ], stderr=self.nulfp)
-            self.converted = True
-            return true
-            
     def tile(self):
         if not self.converted:
             self.convertToASC()
-            
-        try:
-            self.converting.wait()
-        except:
-            pass
             
         self.tiling = subprocess.Popen(
             ["java",
@@ -109,11 +82,25 @@ class Layer():
             self.tileFolder,
             str(self.zoom+1)
             ], stderr=self.nulfp)
+        self.tiling.wait()
         
+    def registerMetadata(self):
+        #send metadata to GAE
+        return True
+        
+    def storeTiles(self):
+        #store tiles in couchdb
+        return True
         
 if __name__ == "__main__":
     #run the code
-    dirName = str(sys.argv[1])
-    fileName = str(sys.argv[2])
-    Layer(dirname=dirname,filename=filename)
-    Layer.tile()
+    filename = str(sys.argv[1])
+    try:
+        dirname = str(sys.argv[2])
+    except:
+        dirname = ""
+        
+    layer = Layer(dirname=dirname,filename=filename)
+    #layer.projectToGMAP()
+    layer.convertToASC()
+    layer.tile()
