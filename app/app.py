84aa9083b8212783d8962d54e7d0aa389be7ef0b
Aaron <eightysteele@gmail.com>
2011-02-20 06:49:22 -0800

    Post commit hook implementation for GitHub push notifications via email

--- app/app.py
+++ app/app.py
@@ -19,7 +19,9 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 import os
-from google.appengine.api import memcache as m
+from google.appengine.api import memcache as m, mail
+import simplejson
+import logging
 
 memcache = m.Client()
 
@@ -48,6 +50,27 @@ class AdminFlushMemcacheHandler(BaseHandler):
             self.response.out.write('Memcache failed to flush')
         else:
             self.response.out.write('Memcache flushed')
+     
+class GitHubPostReceiveHooksHandler(BaseHandler):    
+    
+    # Add your email address here if you want push notifications:
+    SEND_LIST = ['eightysteele@gmail.com']
+    
+    def post(self):
+        payload = self.request.get('payload')
+        json = simplejson.loads(payload)
+        title = '[%s] New GitHub activity - GIT push' % json['repository']['name']
+        body = 'The following commits were just pushed:\n\n'
+        for c in json['commits']:
+            body += '%s\n' % c['message']
+            body += '%s (author)\n' % c['author']['name']
+            body += '%s\n' % c['timestamp']
+            body += '%s\n\n' % c['url']
+        logging.info(body)
+        mail.send_mail(sender="Map of Life <admin@mol-lab.appspotmail.com>",
+              to=', '.join(self.SEND_LIST),
+              subject=title,
+              body=body)     
             
 class SearchHandler(BaseHandler):
     """Handler for the search UI."""
@@ -59,7 +82,8 @@ application = webapp.WSGIApplication(
           ('/search', SearchHandler),
           ('/playground/col', ColPage),
           ('/playground/map', MapPage),
-          ('/admin/flush-memcache', AdminFlushMemcacheHandler)],
+          ('/admin/flush-memcache', AdminFlushMemcacheHandler),
+          ('/hooks/post-commit', GitHubPostReceiveHooksHandler), ],
          debug=True)
 
 def main():
