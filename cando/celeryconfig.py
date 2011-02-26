20a532a3667d7be9f64ff73ca759911ff7f679eb
andrew <andrewxhill@gmail.com>
2011-02-26 13:25:41 -0700

    cando Celery queue mostly in place

---
+++ cando/celeryconfig.py
@@ -0,0 +1,9 @@
+BROKER_HOST = "localhost"
+BROKER_PORT = 5672
+BROKER_USER = "username"
+BROKER_PASSWORD = "password"
+BROKER_VHOST = "molqueue"
+CELERY_IMPORTS = ("tiling.tasks", )
+CELERYD_CONCURRENCY = 4
+CELERY_RESULT_BACKEND = "amqp"
+CELERYD_LOG_FILE = "celery.log"
