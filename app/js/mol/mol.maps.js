acd579be4d4b18cfa1f061a0b6375f6f68cfa51e
Aaron <eightysteele@gmail.com>
2011-03-15 06:33:59 -0700

    Collapsing mol.maps and mol.ui

--- app/js/mol/mol.maps.js
+++ app/js/mol/mol.maps.js
@@ -1,3 +1,67 @@
+// =============================================================================
+// Map object
+
+/**
+ * The Map.
+ * 
+ * @constructor
+ */
+mol.maps.Map = function(context) {
+    if (!(this instanceof mol.maps.Map)) {
+        return new mol.maps.Map();
+    }
+    var contextDoc = document.getElementById($(context).attr('id')),
+        mapDiv = $("#map"),
+        self = this;
+    this.context = context;
+    this.options = {
+        zoom: 2,
+        maxZoom: 20,
+        center: new google.maps.LatLng(0,0),
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    };
+    this.map = new google.maps.Map(mapDiv[0], this.options);
+    this.wireEvents();
+    return this;
+};
+    
+mol.maps.Map.prototype.wireEvents = function() {
+    var self = this;
+    mol.eventBus.bind(
+        mol.event.Types.ADD_CUSTOM_MAP_CONTROL, 
+        function(divId, position) {
+            self.addController(divId, position);
+        }
+    );
+    mol.eventBus.bind(
+        mol.event.Types.ADD_NEW_MAP_LAYER,
+        function(layer, id) {
+            var tmp = self.layers.reverse;
+            tmp.push({'id': id, 'layer': layer});
+            self.layers = tmp.reverse;
+        }
+    );
+    mol.eventBus.bind(
+        mol.event.Types.REORDER_MAP_LAYERS,
+        function(layerOrder) {
+            var tmp = new Array(self.layers.length),
+                ct = 0;
+            for (var i in layerOrder) {
+                tmp[ct] = layerOrder[i];
+                ct++;
+            }
+            self.layers = tmp;
+        }
+    );    
+};
+
+mol.maps.Map.prototype.addController = function(divId, position) {   
+    this.map.controls[position].push(divId[0]);
+};
+
+// =============================================================================
+// Layer object
+    
 /**
  * Constructor for the Layer object.
  * 
@@ -73,4 +137,3 @@ mol.maps.Layer.prototype.showTypesUi = function() {
     );        
    mol.eventBus.trigger(mol.event.Types.ADD_NEW_STACK_LAYER, dialog);                                         
 };
-
