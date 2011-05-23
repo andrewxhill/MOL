71ddaa88ca126c7468862235379a567ee23a66ca
Aaron <eightysteele@gmail.com>
2011-05-23 09:27:21 -0700

    Newly compiled mol.js

--- app/static/js/mol.js
+++ app/static/js/mol.js
@@ -1272,7 +1272,10 @@ MOL.modules.LayerControl = function(mol) {
             },
 
             getPlaceState: function() {
-                return {lv: this._display.isLayersVisible() ? 1 : 0};
+                return {
+                    lv: this._display.isLayersVisible() ? 1 : 0,
+                    layers: _.keys(this._layerIds).join(',')
+                };
             },
              
             /**
@@ -1556,6 +1559,7 @@ MOL.modules.LayerList = function(mol) {
             init: function(api, bus) {
                 this._api = api;
                 this._bus = bus;
+                this._layerIds = [];
             },
 
             /**
@@ -2820,7 +2824,11 @@ MOL.modules.Search = function(mol) {
             go: function(place) {
                 var q = place.q,
                     visible = place.sv ? parseInt(place.sv) : 0,
-                    display = this._display;
+                    display = this._display,
+                    layerKeys = place.layers ? place.layers.split(',') : null,
+                    resultWidget = null,
+                    layerKey = null,
+                    self = this;
                 
                 if (visible) {
                     display.show();
@@ -2828,7 +2836,24 @@ MOL.modules.Search = function(mol) {
 
                 if (q) {
                     display.getSearchBox().val(q);
-                    this._onGoButtonClick();
+                    this._onGoButtonClick(
+                        function() {
+                            if (layerKeys) {
+                                for (k in layerKeys) {
+                                    layerKey = layerKeys[k];
+                                    for (x in self._resultWidgets) {
+                                        resultWidget = self._resultWidgets[x];
+                                        if (resultWidget.key_name === layerKey) {
+                                            resultWidget.widget.getCheckbox().setChecked(true);
+                                        }
+                                    }
+                                }
+                                self._onAddButtonClick();
+                            }
+                            
+                        }
+                        
+                    );
                 }
             },
 
@@ -3133,7 +3158,7 @@ MOL.modules.Search = function(mol) {
                 this._displayPage(layers);
             },
 
-            _onGoButtonClick: function() {
+            _onGoButtonClick: function(cb) {
                 var query = this._display.getSearchBox().val(),
                     LayerAction = mol.ajax.LayerAction,
                     action = new LayerAction('search', {query: query}),
@@ -3154,6 +3179,9 @@ MOL.modules.Search = function(mol) {
                         for (i in filterNames) {
                             fn = filterNames[i];
                             self._createNewFilter(fn,response);
+                        }
+                        if (callback) {
+                            cb();
                         }
                     },
 
