bf067c99f6af6df545f6ecf00bcadb87fbfb688d
andrew <andrewxhill@gmail.com>
2011-06-07 14:31:00 -0600

    Import getting-started version 0.2.0

---
+++ utilities/chef/cookbooks/getting-started/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Opscode, Inc."
+maintainer_email "sysadmin@opscode.com"
+license          "Apache 2.0"
+description      "Used by the Getting Started guide at http://help.opscode.com"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.2.0"
