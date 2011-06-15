309e2c01c6fc3ea3873b96cac559b58919a6309c
andrew <andrewxhill@gmail.com>
2011-06-15 00:44:34 -0600

    remove lines in pa

--- layers/layers/lib/mol/service/GenerateTile.py
+++ layers/layers/lib/mol/service/GenerateTile.py
@@ -57,9 +57,9 @@ class RenderThread:
         s = mapnik.Style()
         r=mapnik.Rule()
         r.symbols.append(mapnik.PolygonSymbolizer(mapnik.Color(params.get('polygon'))))
-        l = mapnik.LineSymbolizer(mapnik.Color(params.get('line')),params.get('line-width'))
+        l = mapnik.LineSymbolizer(mapnik.Color(params.get('line')),1.0)
         l.stroke.opacity = 1.0
-        l.stroke.width = 1.0
+        l.stroke.width = 0.0
         r.symbols.append(l)
         s.rules.append(r)
         self.m.append_style('style',s)
