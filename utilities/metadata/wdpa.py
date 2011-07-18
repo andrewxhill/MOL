9bd16417acc75d430d49cf7fa0b2ddba05e105ec
John Wieczorek <tuco@berkeley.edu>
2011-07-18 08:23:23 -0700

    Adding first draft for WDPA Bioinventory collection and metadata.

---
+++ utilities/metadata/wdpa.py
@@ -0,0 +1,197 @@
+import ogr, glob, os
+import urllib, urllib2
+import simplejson, datetime
+
+class NewProtectedArea():
+    def __init__(self):
+        self.source = None
+        self.type = None
+        self.name = None
+        self.description = None
+        self.extentText = None
+        self.srs = None
+        self.xmin = None
+        self.ymin = None
+        self.xmax = None
+        self.ymax = None
+        self.location = None
+        self.format = None
+        self.geoFormat = None
+        self.geoType = None
+        self.resType = None
+        self.resUnit = None
+        self.resVal = None
+        self.variables = []
+        
+    def newVariable(self, name, value):
+        self.variables.append({
+                            "name": name,
+                            "value": value })
+    def json(self):
+        return  {
+              "source": self.source,
+              "type": self.type,
+              "name": self.name,
+              "description": self.description,
+              "collectionKey": "collection/protectedareas/wdpa/1",
+              "creationDate": "2010",
+              "storage":{
+                 "location": self.location,
+                 "format": self.format,
+                 "uploadDate": str(datetime.datetime.now()),
+              },
+              "temporal": {
+                    "coverage": {
+                        "start": None,
+                        "end": None
+                    }
+              },
+              "spatial":{
+                 "crs":{
+                    "srs": self.srs,
+                    "extent":{
+                       "text":self.extentText,
+                       "coordinates":{
+                          "0": self.xmin,
+                          "1": self.ymin,
+                          "2": self.xmax,
+                          "3": self.ymax
+                       }
+                    },
+                    "format": self.geoFormat,
+                    "type": self.geoType,
+                    "info": {
+                       "resolution":{
+                          "type": self.resType,
+                          "value": self.resVal,
+                          "unit": self.resUnit
+                       },
+                    }
+                 }
+              },
+              "variables": self.variables
+            }
+
+def newMetadata(f):
+    eco_code = f.replace('.shp','')
+    out = NewProtectedArea()
+    out.source = "IUCN and UNEP"
+    out.type = "Protected Area"
+    out.location = "/ftp/pa/shp/%s.shp" % eco_code
+    out.format = "Esri Shapefile"
+    out.geoFormat = "vector"
+    out.geoType = "multipolygon"
+    out.newVariable('wdpaid', wdpaid)
+    
+    ds = ogr.Open ( f )
+    lyr = ds.GetLayerByName( wdpaid )
+    feat_def = lyr.GetLayerDefn()
+    
+    name, country, category, marine = None, None, None, None
+    for feat in lyr:
+        if name is None:
+            name = feat.GetField('NAME')
+        if country is None:
+            country = feat.GetField('COUNTRY')
+        if category is None:
+            category = feat.GetField('IUCNCAT')
+        if marine is None:
+            marine = feat.GetField('MARINE')
+    out.newVariable('NAME', name)
+    out.newVariable('COUNTRY', country)
+    out.newVariable('IUCNCAT', category)
+    out.newVariable('MARINE', marine)
+            
+    out.extentText = country
+    feat = lyr.GetFeature(0)
+    geom = feat.GetGeometryRef()
+    
+    out.name = feat.GetField('NAME')
+    
+    out.srs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+    extent = lyr.GetExtent()
+    out.xmin = extent[0]
+    out.ymin = extent[2]
+    out.xmax = extent[1]
+    out.ymax = extent[3]
+    
+    return out.json()
+
+def newCollection():
+    return {
+          "source":"IUCN and UNEP",
+          "type":"Ecoregion",
+          "name":"World Database on Protected Areas",
+          "description":"An interactive database for protected areas worldwide, reconciling governmental, expert and general public opinions on protected areas. It encompasses the World Database on Protected Areas and provides a platform for the protected area constituency.",
+          "url":"http://www.protectedplanet.net/about",
+          "agreements":{},
+          "creationDate": "2010",
+          "uploadDate": str(datetime.datetime.now()),
+          "changeDate": "2010",
+          "allowed_uses":{
+              "visualization": "unknown",
+              "download": "unknown",
+             },
+              "references":{
+                 "0":{
+                    "authors":"IUCN and UNEP",
+                    "year":2010,
+                    "publication":"UNEP-WCMC. Cambridge, UK. www.protectedplanet.net",
+                    "title":"The World Database on Protected Areas (WDPA)"
+                    }
+                 },
+             "spatial":{
+                "crs":{
+                   "srs": "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs",
+                   "extent":{
+                      "text":"Global",
+                      "coordinates":{
+                         "0": -90.0,
+                         "1": -180.0,
+                         "2": 90.0,
+                         "3": 180.0
+                      }
+                   },
+                   "format": "vector",
+                   "type": "multipolygon",
+                   "info": {
+                        "resolution": {
+                            "type": None,
+                            "value": None,
+                            "unit": None
+                        }
+                    },
+                },
+            },
+        }
+        
+if __name__== '__main__':
+    os.chdir("/ftp/pa/shp")
+    url = 'http://axh.mol-lab.appspot.com/andrew'
+    #url = 'http://localhost:8080/andrew'
+    #os.chdir("/home/andrew/Documents")
+    values = {'payload' : simplejson.dumps(newCollection()),
+              'key_name' : 'collection/protectedareas/wdpa/1'}
+    data = urllib.urlencode(values)
+    req = urllib2.Request(url, data)
+    response = urllib2.urlopen(req)
+    the_page = response.read()
+    
+    for f in glob.glob("*.shp"):
+        #if f == 'AA0101.shp':
+        c = f.replace('.shp', '')
+        out = newMetadata(f)
+        values = {'payload' : simplejson.dumps(out),
+                  'key_name' : 'protectedareas/wdpa/%s' % c,
+                  'parent_key_name' : 'protectedareas/wdpa/%s' % c,
+                  'parent_kind' : 'MultiPolygon'}
+        try:
+            data = urllib.urlencode(values)
+            req = urllib2.Request(url, data)
+            response = urllib2.urlopen(req)
+            the_page = response.read()
+        except:
+            print c
+            
+    
+    
